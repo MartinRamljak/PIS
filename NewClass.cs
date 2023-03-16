622b2f7c492e38ca588522304093e9369b3330bd
tomicmateo <92399787+tomicmateo@users.noreply.github.com>
2023-03-16 19:17:40 +0100

    Update NewClass.cs

--- NewClass.cs
+++ NewClass.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace vjezbaaa1
+{
+    class NewClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suradnja");
+        }
+    }
+}
